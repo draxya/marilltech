5e07e0374eef7a16b53af78d57f814f8caa78897
draxya <68575901+draxya@users.noreply.github.com>
2022-05-05 14:23:14 +0300

    Production

---
+++ src/components/layout/Footer.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import Logo from './partials/Logo';
+import FooterSocial from './partials/FooterSocial';
+
+const propTypes = {
+  topOuterDivider: PropTypes.bool,
+  topDivider: PropTypes.bool
+}
+
+const defaultProps = {
+  topOuterDivider: false,
+  topDivider: false
+}
+
+const Footer = ({
+  className,
+  topOuterDivider,
+  topDivider,
+  ...props
+}) => {
+
+  const classes = classNames(
+    'site-footer center-content-mobile',
+    topOuterDivider && 'has-top-divider',
+    className
+  );
+
+  return (
+    <footer
+      {...props}
+      className={classes}
+    >
+      <div className="container">
+        <div className={
+          classNames(
+            'site-footer-inner',
+            topDivider && 'has-top-divider'
+          )}>
+          <div className="footer-top space-between text-xxs">
+            <Logo />
+            <div className="footer-copyright">Marill ROV Team, 2022.</div>
+            <FooterSocial />
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+}
+
+Footer.propTypes = propTypes;
+Footer.defaultProps = defaultProps;
+
+export default Footer;
---
+++ src/views/Home.js
@@ -0,0 +1,20 @@
+import React from 'react';
+// import sections
+import Hero from '../components/sections/Hero';
+import FeaturesTiles from '../components/sections/FeaturesTiles';
+import FeaturesSplit from '../components/sections/FeaturesSplit';
+import Testimonial from '../components/sections/Testimonial';
+
+const Home = () => {
+
+  return (
+    <>
+      <Hero className="illustration-section-01" />
+      <FeaturesTiles />
+      <FeaturesSplit invertMobile topDivider imageFill className="illustration-section-02" />
+      <Testimonial topDivider />
+    </>
+  );
+}
+
+export default Home;
