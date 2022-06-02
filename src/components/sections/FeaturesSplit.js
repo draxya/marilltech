971340fda45c1f459e60eb7370f94fa0da1ac27c
draxya <68575901+draxya@users.noreply.github.com>
2022-06-02 18:03:14 +0300

    Update FeaturesSplit.js

--- src/components/sections/FeaturesSplit.js
+++ src/components/sections/FeaturesSplit.js
@@ -61,7 +61,7 @@ const FeaturesSplit = ({
     >
       <div className="container">
         <div className={innerClasses}>
-          <SectionHeader data={sectionHeader} className="center-content text-color-primary" />
+          <SectionHeader data={sectionHeader} className="center-content" />
           <div className={splitClasses}>
 
             <div className="split-item">
